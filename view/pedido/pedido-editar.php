792ee268df27b93cd5e2574a5e7f84842029a4ca
ElmerYam <elmeryam5@gmail.com>
2019-05-31 16:23:27 -0500

    peticion de agregar pedido

--- view/pedido/pedido-editar.php
+++ view/pedido/pedido-editar.php
@@ -1,45 +1,45 @@
 <h1 class="page-header">
     <?php  
     $test = $pedido->datos[0];    
-    echo $test->folio != null ? $test->folio : 'Nuevo Registro'; ?>
+    echo $pedido->folio != null ? $pedido->folio : 'Nuevo Registro'; ?>
 </h1>
 
 <ol class="breadcrumb">
   <li><a href="?c=Pedido">Pedidos</a></li>
-  <li class="active"><?php echo $test->folio != null ? $test->id_cte : 'Nuevo Registro'; ?></li>
+  <li class="active"><?php echo $pedido->folio != null ? $pedido->id_cte : 'Nuevo Registro'; ?></li>
 </ol>   
 
 <form id="frm-alumno" action="?c=Pedido&a=Guardar" method="post" enctype="multipart/form-data">
-    <input type="text" name="folio" value="<?php echo $test->folio; ?>" class="form-control" disabled/>
+    <input type="text" name="folio" value="<?php echo $pedido->folio; ?>" class="form-control" disabled/>
     
     <div class="form-group">
         <label>Id cliente</label>
-        <input type="text" name="id_cte" value="<?php echo $test->id_cte; ?>" class="form-control" placeholder="Ingrese su nombre" data-validacion-tipo="requerido|min:3" />         
+        <input type="text" name="id_cte" value="<?php echo $pedido->id_cte; ?>" class="form-control" placeholder="Ingrese su nombre" data-validacion-tipo="requerido|min:3" />         
     </div>
     
     <div class="form-group">
         <label>Id estab</label>
-        <input type="text" name="id_estab" value="<?php echo $test->id_estab; ?>" class="form-control" placeholder="Ingrese su apellido" data-validacion-tipo="requerido|min:10" />
+        <input type="text" name="id_estab" value="<?php echo $pedido->id_estab; ?>" class="form-control" placeholder="Ingrese su apellido" data-validacion-tipo="requerido|min:10" />
     </div>
     
     <div class="form-group">
         <label>hora solicitud</label>
-        <input type="text" name="hora_solicitud" value="<?php echo $test->hora_solicitud; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
+        <input type="text" name="hora_solicitud" value="<?php echo $pedido->hora_solicitud; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
     </div>
 
     <div class="form-group">
         <label>Status pedido</label>
-        <input type="text" name="status_pedido" value="<?php echo $test->status_pedido; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
+        <input type="text" name="status_pedido" value="<?php echo $pedido->status_pedido; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
     </div>
 
     <div class="form-group">
         <label>Forma de pago</label>
-        <input type="text" name="forma_pago" value="<?php echo $test->forma_pago; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
+        <input type="text" name="forma_pago" value="<?php echo $pedido->forma_pago; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
     </div>
 
     <div class="form-group">
         <label>Total</label>
-        <input type="text" name="total" value="<?php echo $test->total; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
+        <input type="text" name="total" value="<?php echo $pedido->total; ?>" class="form-control" placeholder="Ingrese su correo electrónico" data-validacion-tipo="requerido|email" />
     </div>
             
     
