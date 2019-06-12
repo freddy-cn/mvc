345e7998c92c08bec9460fdc7945f92344f7acea
ElmerYam <elmeryam5@gmail.com>
2019-06-11 21:24:40 -0500

    creacion de modelo y controlador de establecimientos

---
+++ controller/establecimientos.controller.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'model/establecimiento.php';
+
+class EstablecimientosController{
+
+    private $model;
+
+    public function __CONSTRUCT(){
+        $this->model = new Establecimiento();
+    }
+
+    public function Index(){
+        require_once 'view/header.php';
+        require_once 'view/pedido/pedido.php';
+        require_once 'view/footer.php';
+    }
+
+    public function Crud(){
+        $pedido = new Establecimiento();
+        if(isset($_REQUEST['folio'])){
+            $datos = $this->model->Obtener($_REQUEST['folio'])->datos[0];
+            $pedido->folio = $datos->folio;
+            $pedido->id_cte = $datos->id_cte;
+            $pedido->id_estab = $datos->id_estab;
+            $pedido->hora_solicitud = $datos->hora_solicitud;
+            $pedido->status_pedido = $datos->status_pedido;
+            $pedido->forma_pago = $datos->forma_pago;
+            $pedido->total = $datos->total;
+            //$pedido->folio = $this->model->Obtener($_REQUEST['folio'])->folio;
+        }
+
+        require_once 'view/header.php';
+        require_once 'view/pedido/pedido-editar.php';
+        require_once 'view/footer.php';
+    }
+
+    public function Guardar(){
+        $pedido = new Establecimiento();
+
+        $folio = $_REQUEST['folio'];
+        $pedido->id_cte = $_REQUEST['id_cte'];
+        $pedido->id_estab = $_REQUEST['id_estab'];
+        $pedido->hora_solicitud = $_REQUEST['hora_solicitud'];
+        $pedido->status_pedido = $_REQUEST['status_pedido'];
+        $pedido->forma_pago = $_REQUEST['forma_pago'];
+        $pedido->total = $_REQUEST['total'];
+
+        $datos_json = json_encode(
+            array(
+                //'folio' => $pedido->folio,
+                'id_cte' => $pedido->id_cte,
+                'id_estab' =>$pedido->id_estab,
+                'hora_solicitud'=>$pedido->hora_solicitud,
+                'status_pedido'=>$pedido->status_pedido,
+                'forma_pago'=>$pedido->forma_pago,
+                'total'=>$pedido->total
+            )
+        );
+        $folio > 0
+            ? $this->model->Actualizar($datos_json,$folio)
+            : $this->model->Registrar($datos_json);
+
+        header('Location: index.php');
+    }
+
+    public function Eliminar(){
+        $this->model->Eliminar($_REQUEST['folio']);
+        header('Location: index.php');
+    }
+
+    public function logout(){
+        session_unset();
+        session_destroy();
+        header('Location: login.php');
+    }
+
+    //creacion del reporte
+    public function Reporte(){
+        require_once 'model/reportes.php';
+    }
+}
---
+++ model/establecimiento.php
@@ -0,0 +1,124 @@
+<?php
+include_once('./httpful.phar');
+class Establecimiento
+{
+	private $pdo;
+
+	public $folio;
+	public $id_cte;
+	public $id_estab;
+	public $hora_solicitud;
+	public $status_pedido;
+	public $forma_pago;
+	public $total;
+
+	public $response;
+	public $key;
+
+	public function __CONSTRUCT()
+	{
+		try
+		{
+			$this->pdo = Database::StartUp();
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Listar()
+	{
+		try
+		{
+			$key =  $_SESSION['claveApi'];
+			$url = 'http://localhost/webserviceRestaurantes/pedidos';
+
+			$response = \Httpful\Request::get($url)
+			->addHeader('authorization', $key)
+			->send();
+
+			return json_decode($response);
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Obtener($folio)
+	{
+		try
+		{
+			$key =  $_SESSION['claveApi'];
+			$url = 'http://localhost/webserviceRestaurantes/pedidos/'.$folio;
+
+			$response = \Httpful\Request::get($url)
+			->addHeader('authorization', $key)
+			->send();
+			return json_decode($response);
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Eliminar($folio)
+	{
+
+		try
+		{
+			$key =  $_SESSION['claveApi'];
+			$url = 'http://localhost/webserviceRestaurantes/pedidos/'.$folio;
+
+			$response = \Httpful\Request::delete($url)
+			->addHeader('authorization', $key)
+			->send();
+			return json_decode($response);
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+
+	}
+
+	public function Actualizar($data,$folio)
+	{
+		try
+		{
+			$key =  $_SESSION['claveApi'];
+			$url = 'http://localhost/webserviceRestaurantes/pedidos/'.$folio;
+
+			$response = \Httpful\Request::put($url)
+			->addHeader('authorization', $key)
+			->body($data)
+			->send();
+
+			return json_decode($response);
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Registrar($data)
+	{
+		try
+		{
+			$key =  $_SESSION['claveApi'];
+			$url = 'http://localhost/webserviceRestaurantes/pedidos';
+
+			$response = \Httpful\Request::post($url)
+			->addHeader('authorization', $key)
+			->body($data)
+			->send();
+
+			return json_decode($response);
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+}
