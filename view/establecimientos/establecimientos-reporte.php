1076e2b4a072128467a6ffde450b5181a02bfc88
ElmerYam <elmeryam5@gmail.com>
2019-06-13 16:19:27 -0500

    mejora del reporte; reporte con tres tablas

--- view/establecimientos/establecimientos-reporte.php
+++ view/establecimientos/establecimientos-reporte.php
@@ -18,7 +18,7 @@ $link = mysqli_connect('localhost','root','','rest');
 $pdf = new PDF();
 $pdf->AddPage("L");
 // First table: output all columns
-$pdf->Table($link,'SELECT establecimientos.id_estab, establecimientos.nombre_estab, establecimientos.calle_estab, establecimientos.colonia_estab, establecimientos.ciudad_estab, establecimientos.telefono_estab, establecimientos.num_exterior_estab FROM establecimientos');
+$pdf->Table($link,'SELECT * FROM reporte');
 $pdf->Image('logo1.png',10,2,-200);
 $pdf->Output();
 ?>
